559ec7cad8e5f061f26129312831e0701e358775
azaika <takaaza@googlemail.com>
2015-06-29 02:37:21 +0900

    サンプルプログラムを修正

--- wawl/Source.cpp
+++ wawl/Source.cpp
@@ -16,7 +16,7 @@ void WawlMain() {
 		else if (Keyboard::Get(Key::One).Clicked())
 			Mouse::SetPos({ 100, 100 });
 		else if (Keyboard::Get(Key::Two).Clicked())
-			sys::RunCmdLine(L"notepad C:\\Users\\takashi\\Desktop\\test.txt", false);
+			sys::RunCmdLine(L"cmd /k ipconfig", true);
 
 	}
 }
